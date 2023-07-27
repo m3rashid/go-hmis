4d690653ee8de908baec7a7fa2209848bddc2b54
m3rashid <m3rashid.hussain@gmail.com>
2023-07-27 12:07:26 +0530

    init

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/redis/go-redis/v9 v9.0.5
+	golang.org/x/crypto v0.7.0
 )
 
 require (
--- modules/auth/controllers.go
+++ modules/auth/controllers.go
@@ -1,10 +1,12 @@
 package auth
 
 import (
+	"go-hmis/db"
 	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var DbName string = os.Getenv("MONGO_DB_NAME")
@@ -24,6 +26,21 @@ func LoginHandler(c *fiber.Ctx) error {
 }
 
 func CreateAccountHandler(c *fiber.Ctx) error {
+	user := db.CreateUserParams{}
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+		})
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to create user",
+		})
+	}
+
+	user.Password = string(password)
 
 	return c.SendStatus(200)
 }
